90ef67ed67e1e46da27ce756c3370e054c718b49
farice9 <limyb50@gmail.com>
2020-08-20 15:36:12 +0800

    add greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,19 @@
 public class Duke {
     public static void main(String[] args) {
+        greet();
+    }
+
+    // Level 0 : Basic greeting - Duke greets user and exit
+    public static void greet(){
+        System.out.println("--------------------------------------");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("--------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    // Default : Print Duke logo + greet
+    public static void logo(){
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
