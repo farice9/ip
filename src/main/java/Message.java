d32ff50a508aee78380b0f89d92e54616d06e34d
farice9 <limyb50@gmail.com>
2020-09-29 21:27:49 +0800

    Update documentation

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -2,9 +2,7 @@
  * Contains messages to be printed in the user interface
  */
 public class Message {
-    /**
-     * Messages involved when loading data file
-     */
+    // Messages involved when loading data file
     public static final String LOADING_ERROR = "Oh no! There's a problem loading file content";
     public static final String LOADING_FILE = "Loading your previous tasks, hold on! ...";
     public static final String DONE_LOADING = "... Storage file content loaded!";
@@ -14,15 +12,11 @@ public class Message {
     public static final String WRITE_FILE_ERROR = "There's a problem with writing the file";
     public static final String CREATE_NEW_FILE =  "File not found, creating one now!";
 
-    /**
-     * Simple interaction messages
-     */
+    // Simple interaction messages
     public static final String GREET = "Hey mate! Nice to meet you, I'm Duke!\nHow can I help you?\n";
     public static final String GOODBYE = "Goodbye & have a nice day! Hope to see you again!";
 
-    /**
-     * Error messages when detecting task input
-     */
+    // Error messages when detecting task input
     public static final String INVALID_TASK_TYPE_ERROR = "☹ Sorry but I don't understand that task at all. Try again?";
     public static final String EMPTY_TODO_ERROR = "☹ OH NO! The description of todo cannot be empty!";
     public static final String EMPTY_DEADLINE_DESCRIPTION_ERROR = "☹ OH NO! The description of deadline cannot be empty!";
@@ -30,27 +24,19 @@ public class Message {
     public static final String EMPTY_EVENT_DESCRIPTION_ERROR = "☹ OH NO! The description of event cannot be empty!";
     public static final String EMPTY_EVENT_DATE_ERROR = "No date is found for this event! Try adding a date after /at";
 
-    /**
-     * Error messages when using "delete" function
-     */
+    // Error messages when using "delete" function
     public static final String DELETE_TASK_NOT_FOUND_ERROR = "Task not found! Nothing is there to be deleted";
     public static final String INDEX_INPUT_NOT_DETECTED_ERROR = "No index number input detected. Please try again!";
 
-    /**
-     * Messages when using "list" function
-     */
+    // Messages when using "list" function
     public static final String EMPTY_TASK_LIST_MESSAGE = "No tasks has been added yet. Try adding something!";
 
-    /**
-     * Messages when using "done" function
-     */
+    // Messages when using "done" function
     public static final String INVALID_DONE_INDEX_ERROR = "Task not found. Nothing is marked as done. Check your task index number input!";
     public static final String TASK_ALREADY_DONE_MESSAGE = "This task has already been done! Good luck completing others!!!";
     public static final String MARK_AS_DONE_MESSAGE = "Good job! I have marked this task as done:";
 
-    /**
-     * Graphics involved in the UI
-     */
+    // Graphics involved in the UI
     public static final String LINE_DIVIDER = "*******************************************************************************";
 
     public static final String USER_COMMAND_GUIDE =
